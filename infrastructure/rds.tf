dfb907a1eb506ff3d0daa5a5beb707cd90c7c549
AyoDennis <ayodejidennis33@gmail.com>
2025-07-02 22:49:54 +0100

    create internet gateway for public access

--- infrastructure/rds.tf
+++ infrastructure/rds.tf
@@ -47,3 +47,9 @@ resource "aws_route_table" "public_route_table" {
   }
 }
 
+# Associate the public subnet with the public route table
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
