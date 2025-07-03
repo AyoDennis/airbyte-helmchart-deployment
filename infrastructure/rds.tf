1fcf379fcd00565791504a18244c4f079f35781a
AyoDennis <ayodejidennis33@gmail.com>
2025-07-03 08:05:13 +0100

    aws security group rules

--- infrastructure/rds.tf
+++ infrastructure/rds.tf
@@ -75,4 +75,15 @@ resource "aws_security_group" "rds_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "rds_sg"
+  }
+}
+
