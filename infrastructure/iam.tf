7c6dd5786365e6751a888b2afc085ac28fbfe8c6
AyoDennis <ayodejidennis33@gmail.com>
2025-07-03 08:30:45 +0100

    airbyte access key

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -10,3 +10,9 @@ resource "aws_iam_user" "airbyte_user" {
 resource "aws_iam_access_key" "airbyte_credentials" {
   user = aws_iam_user.airbyte_user.name
 }
+
+resource "aws_ssm_parameter" "airbyte_access_key" {
+  name  = "/dev/airbyte/airbyte_user_access_key"
+  type  = "String"
+  value = aws_iam_access_key.airbyte_credentials.id
+}
