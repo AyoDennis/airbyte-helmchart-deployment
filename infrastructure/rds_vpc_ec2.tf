b78b44b14e8e0fc01661d25ce1fb154cff079ff8
AyoDennis <ayodejidennis33@gmail.com>
2025-07-11 10:54:19 +0100

    airbyte server

--- infrastructure/rds_vpc_ec2.tf
+++ infrastructure/rds_vpc_ec2.tf
@@ -125,3 +125,14 @@ resource "aws_db_instance" "rds_instance" {
 
 }
 
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.amzn-linux-2023-ami.id
+  instance_type = "c6a.2xlarge"
+  subnet_id     = aws_subnet.example.id
+
+
+  tags = {
+    Name = "airbyte-server"
+  }
+}
